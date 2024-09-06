79d7fe97ebfdec90e93500da72c58dfc887f0888
fletch4503 <fletch4503@gmail.com>
2024-09-06 13:07:39 +0300

    Connect to db and making views for db elements

--- todo_manager/todo_list/views.py
+++ todo_manager/todo_list/views.py
@@ -1,3 +1,17 @@
+from django.http import (
+    HttpRequest,
+    HttpResponse,
+)
 from django.shortcuts import render
 
+from .models import ToDoItem
+
+
 # Create your views here.
+def index_view(request: HttpRequest) -> HttpResponse:
+    todo_items = ToDoItem.objects.order_by("id").all()  # свойство objects есть в БД сортировкой по id
+    return render(
+        request,
+        template_name="todo_list/index.html",
+        context={"todo_items": todo_items}
+    )
