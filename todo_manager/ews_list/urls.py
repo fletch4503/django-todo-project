e515bd7e9fe8c476e74599d010b48dc8315a6cb5
fletch4503 <fletch4503@gmail.com>
2024-09-11 17:36:32 +0300

    Add table view in ews and todo apps. issue - disappear links on detail view

--- todo_manager/ews_list/urls.py
+++ todo_manager/ews_list/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 # from django.views.generic import TemplateView
 
 from . import views
@@ -7,7 +7,7 @@ app_name = "ews_list"
 
 urlpatterns = [
     path("", views.EWSListIndexView.as_view(), name="index"),
-    path("about/", views.about, name="about"),
+    re_path(r'about', views.about, name="about"),
     path("<int:pk>/", views.EWSDetailView.as_view(), name="detail"),
     path("list/", views.EWSListView.as_view(), name="list"),
     path("done/", views.EWSListDoneView.as_view(), name="done"),
--- todo_manager/todo_list/urls.py
+++ todo_manager/todo_list/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 # from django.views.generic import TemplateView
 
 from . import views
@@ -7,7 +7,7 @@ app_name = "todo_list"
 
 urlpatterns = [
     path("", views.ToDoListIndexView.as_view(), name="index"),
-    path("about/", views.about, name="about"),
+    re_path(r'about', views.about, name="about"),
     path("<int:pk>/", views.ToDoDetailView.as_view(), name="detail"),  # Детальный вид по элементу из списка
     path("list/", views.ToDoListView.as_view(), name="list"),  # Список всех дел
     path("done/", views.ToDoListDoneView.as_view(), name="done"),  # Сделанные дела
